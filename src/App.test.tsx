8dc7eaaabea644e2aee2ea64836154cbca3f985a
dakota002 <dakota.d@live.com>
2023-02-27 21:33:49 -0500

    test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("does nothing", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const plainElement = screen.getByText(/Working on it/i);
+  expect(plainElement).toBeInTheDocument();
 });
